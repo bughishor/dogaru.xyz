8ad0656606406e093473f2598c8e376b7c0ae785
Bogdan Dogaru <bogdan5dogaru@gmail.com>
2020-09-25 14:36:57 +0100

    Added missing favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import '../style/main.scss';
 
 export default () => {
   const { title, lang, description } = headData;
-  const socialImage = useStaticQuery(query);
+  const { logo, favicon } = useStaticQuery(query);
 
   return (
     <>
@@ -17,7 +17,8 @@ export default () => {
         <title>{title || 'Python & Front End Developer'}</title>
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Python & Front End Developer'} />
-        <meta property="og:image" content={socialImage.file.childImageSharp.fixed.src} />
+        <meta property="og:image" content={logo.childImageSharp.fixed.src} />
+        <link rel="icon" type="image/png" href={favicon.childImageSharp.fixed.src}/>
       </Helmet>
       <App />
     </>
@@ -25,10 +26,17 @@ export default () => {
 };
 
 const query = graphql`
-  query socialImage {
-    file(relativePath: { eq: "bogdan-dogaru-web-developer.jpg" }) {
+  query images {
+    logo: file(relativePath: { eq: "bogdan-dogaru-web-developer.jpg" }) {
       childImageSharp  {
         fixed(width: 1200, quality: 100) {
+          src
+        }
+      }
+    }
+    favicon: file(relativePath: { eq: "favicon.png" }) {
+      childImageSharp  {
+        fixed(width: 512, quality: 100) {
           src
         }
       }
